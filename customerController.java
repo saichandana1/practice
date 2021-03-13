938676ad9af2fd70ed2064b202daa046f4c8a640
saichandana1 <chandananimmagadda66@gmail.com>
2021-03-13 13:31:12 +0530

    secondcommit

---
+++ customerController.java
@@ -0,0 +1,101 @@
+/*class customer{
+String customerId;
+String firstName;
+String middleName;
+String lastName;
+long phone;
+long adhar;
+String address;
+
+public void setCustomerId(String cId){
+customerId = cId;
+}
+public void setFirstName(String fN){
+firstName = fN;
+}
+public void setMiddleName(String mN){
+middleName = mN;
+}
+public void setLastName(String lN){
+lastName = lN;
+}
+public void setPhone(long ph){
+phone = ph;
+}
+public void setAdhar(long a){
+adhar = a;
+}
+public void setAddress(String ad){
+address = ad;
+}
+}
+public class customerContoller{
+public static void main(String args[]){
+customer c1 = new customer();
+c1.setCustomerId("c11");
+c1.setFirstName("ad");
+c1.setMiddleName("cb");
+c1.setLastName("gf");
+c1.setPhone(9666667789L);
+c1.setAdhar(2345678L);
+c1.setAddress("sedtg");
+System.out.println(c1.customerId);
+System.out.println(c1.firstName);
+System.out.println(c1.middleName);
+System.out.println(c1.lastName);
+System.out.println(c1.phone);
+System.out.println(c1.adhar);
+System.out.println(c1.address);
+}
+}*/
+
+class customer{
+String customerId = "abc";
+String firstName;
+String middleName;
+String lastName;
+long phone;
+long adhar;
+String address;
+
+/*public void setCustomerId(String cId){
+customerId = cId;
+}
+public void setFirstName(String fN){
+firstName = fN;
+}
+public void setMiddleName(String mN){
+middleName = mN;
+}
+public void setLastName(String lN){
+lastName = lN;
+}
+public void setPhone(long ph){
+phone = ph;
+}
+public void setAdhar(long a){
+adhar = a;
+}
+public void setAddress(String ad){
+address = ad;
+}*/
+}
+class customerContoller{
+public static void main(String args[]){
+customer c1 = new customer();
+/*c1.setCustomerId("c11");
+c1.setFirstName("ad");
+c1.setMiddleName("cb");
+c1.setLastName("gf");
+c1.setPhone(9666667789L);
+c1.setAdhar(2345678L);
+c1.setAddress("sedtg");*/
+System.out.println(c1.customerId);
+System.out.println(c1.firstName);
+System.out.println(c1.middleName);
+System.out.println(c1.lastName);
+System.out.println(c1.phone);
+System.out.println(c1.adhar);
+System.out.println(c1.address);
+}
+}
